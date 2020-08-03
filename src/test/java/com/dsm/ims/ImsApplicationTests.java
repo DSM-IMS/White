ad3e0cf6dc3e25d25942d46a5d2b1253e9931fc5
Lee-Jin-Hyeok <jinjin0816@dsm.hs.kr>
2020-08-03 18:58:31 +0900

    [INIT] project base

---
+++ src/test/java/com/dsm/ims/ImsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsm.ims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
